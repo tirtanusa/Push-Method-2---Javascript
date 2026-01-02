c341791b5a074730d7e4e85f3db01abf493ce0e7
Tirtanusa Kurnia Adhi Perdana <98889173+tirtanusa@users.noreply.github.com>
2026-01-02 17:16:35 +0700

    Penyelesaian calculateTransactionPart2

--- calculateTransactionPart2.js
+++ calculateTransactionPart2.js
@@ -15,22 +15,55 @@
   - tampikan expected output dengan console.log()
 */
 
-function totalPendapatan(transactions) { // kofigurasi parameter sesuai kebutuhan anda
+function totalPendapatan(transactions) {
+  // kofigurasi parameter sesuai kebutuhan anda
   // masukan logika disini
-  
-  return // kembalikan hasil logika disini
+  let jumlah = 0;
+  for (let i = 0; i < transactions.length; i++) {
+    jumlah += transactions[i].harga * transactions[i].jumlah;
+  }
+
+  return jumlah; // kembalikan hasil logika disini
 }
 
-function produkTerlaris(transactions) { // kofigurasi parameter sesuai kebutuhan anda
- // masukan logika disini
-  
- return // kembalikan hasil logika disini
+function produkTerlaris(transactions) {
+  // kofigurasi parameter sesuai kebutuhan anda
+  // masukan logika disini
+  let arrProduk = [
+    { produk: "Baju", jumlah: 0 },
+    { produk: "Celana", jumlah: 0 },
+    { produk: "Sepatu", jumlah: 0 },
+    { produk: "Topi", jumlah: 0 },
+  ];
+
+  for (let i = 0; i < transactions.length; i++) {
+    for (let j = 0; j < arrProduk.length; j++) {
+      if (arrProduk[j].produk == transactions[i].produk) {
+        arrProduk[j].jumlah += transactions[i].jumlah;
+      }
+    }
+  }
+
+  let produkTerlaris = arrProduk[0];
+  for (let i = 0; i < arrProduk.length; i++) {
+    if (arrProduk[i].jumlah > produkTerlaris.jumlah) {
+      produkTerlaris = arrProduk[i];
+    }
+  }
+
+  return produkTerlaris.produk; // kembalikan hasil logika disini
 }
 
-function rataRataHarga(transactions) { // kofigurasi parameter sesuai kebutuhan anda
+function rataRataHarga(transactions) {
+  // kofigurasi parameter sesuai kebutuhan anda
   /// masukan logika disini
-  
-  return // kembalikan hasil logika disini
+  let totalHarga = 0;
+  for (let i = 0; i < transactions.length; i++) {
+    totalHarga += transactions[i].harga;
+  }
+
+  let rataRata = totalHarga / transactions.length;
+  return rataRata; // kembalikan hasil logika disini
 }
 
 // CASE VALUE
@@ -44,10 +77,10 @@ const transactions = [
   { tanggal: "2023-04-15", produk: "Baju", jumlah: 3, harga: 150000 },
   { tanggal: "2023-05-05", produk: "Sepatu", jumlah: 1, harga: 300000 },
   { tanggal: "2023-05-10", produk: "Topi", jumlah: 4, harga: 50000 },
-  { tanggal: "2023-06-15", produk: "Celana", jumlah: 5, harga: 200000 }
+  { tanggal: "2023-06-15", produk: "Celana", jumlah: 5, harga: 200000 },
 ];
 
 // TEST CASE
-console.log("Total Saldo Akhir :", totalPendapatan(transactions));
+console.log("Total Pendapatan :", totalPendapatan(transactions));
 console.log("Transaksi Terbanyak :", produkTerlaris(transactions));
-console.log("Total Transaksi Januari :", rataRataHarga(transactions, "Januari"));
+console.log("Rata-rata Harga Produk :", rataRataHarga(transactions));
